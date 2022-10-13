3a667f646e4e82eec53ca6c49e31324d85fee3e7
Mikhail Melnik <by.zumzoom@gmail.com>
2022-10-13 12:27:53 +0400

    move mocks to contracts

--- contracts/mocks/ERC20DelegatableMock.sol
+++ contracts/mocks/ERC20DelegatableMock.sol
@@ -3,7 +3,7 @@
 pragma solidity 0.8.17;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "../../contracts/ERC20Delegatable.sol";
+import "../ERC20Delegatable.sol";
 
 contract ERC20DelegatableMock is ERC20Delegatable, Ownable {
     constructor(string memory name, string memory symbol, uint256 maxUserFarms)
--- contracts/mocks/WrongDelegation.sol
+++ contracts/mocks/WrongDelegation.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-import "../../contracts/interfaces/IDelegationTopic.sol";
+import "../interfaces/IDelegationTopic.sol";
 
 contract WrongDelegation is IDelegationTopic, ERC20, Ownable {
     error DelegationContractRevert();
