e8c5e3f3a3684b5fdc69a96b0419d1f05fe65db8
Mikhail Melnik <by.zumzoom@gmail.com>
2022-11-14 01:17:55 +0400

    disable more ERC20 methods

--- contracts/DelegatedShare.sol
+++ contracts/DelegatedShare.sol
@@ -9,7 +9,6 @@ import "./interfaces/IDelegatedShare.sol";
 contract DelegatedShare is IDelegatedShare, ERC20Pods, Ownable {
     error ApproveDisabled();
     error TransferDisabled();
-    error TransferFromDisabled();
 
     constructor(
         string memory name,
@@ -40,6 +39,14 @@ contract DelegatedShare is IDelegatedShare, ERC20Pods, Ownable {
     }
 
     function transferFrom(address /* from */, address /* to */, uint256 /* amount */) public pure override(IERC20, ERC20) returns (bool) {
-        revert TransferFromDisabled();
+        revert TransferDisabled();
+    }
+
+    function increaseAllowance(address /* spender */, uint256 /* addedValue */) public pure override returns (bool) {
+        revert ApproveDisabled();
+    }
+
+    function decreaseAllowance(address /* spender */, uint256 /* subtractedValue */) public pure override returns (bool) {
+        revert ApproveDisabled();
     }
 }
