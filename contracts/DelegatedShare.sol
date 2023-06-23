214cdd3192cea128c5ccda6cf1621a3d4341f7c4
Edozie I <112929878+zodenode@users.noreply.github.com>
2023-06-23 12:38:25 +0400

    Update DelegatedShare.sol
    Natspec

--- contracts/DelegatedShare.sol
+++ contracts/DelegatedShare.sol
@@ -5,18 +5,26 @@ pragma solidity ^0.8.0;
 import "@1inch/erc20-pods/contracts/ERC20Pods.sol";
 import "./interfaces/IDelegatedShare.sol";
 
+/// @title DelegatedShare
+/// @dev DelegatedShare is a specialized version of an ERC20 token with additional functionalities.
 contract DelegatedShare is IDelegatedShare, ERC20Pods {
     error ApproveDisabled();
     error TransferDisabled();
     error NotOwnerPod();
 
+    /// @notice The address of the owner pod.
     address immutable public ownerPod;
 
+    /// @dev Throws if called by any account other than the ownerPod.
     modifier onlyOwnerPod {
         if (msg.sender != ownerPod) revert NotOwnerPod();
         _;
     }
 
+    /// @param name_ The name of the token.
+    /// @param symbol_ The symbol of the token.
+    /// @param maxUserPods_ The maximum number of user pods.
+    /// @param podCallGasLimit_ The gas limit for pod calls.
     constructor(
         string memory name_,
         string memory symbol_,
@@ -26,19 +34,34 @@ contract DelegatedShare is IDelegatedShare, ERC20Pods {
         ownerPod = msg.sender;
     }
 
+    /// @notice Add default farm for an account if it doesn't exist.
+    /// @dev Only callable by the owner pod.
+    /// @param account The account to add default farm for.
+    /// @param farm The farm to add.
     function addDefaultFarmIfNeeded(address account, address farm) external onlyOwnerPod {
         if (!hasPod(account, farm)) {
             _addPod(account, farm);
         }
     }
 
+    /// @notice Mint tokens.
+    /// @dev Only callable by the owner pod.
+    /// @param account The address to mint tokens to.
+    /// @param amount The amount of tokens to mint.
     function mint(address account, uint256 amount) external onlyOwnerPod {
         _mint(account, amount);
     }
 
+    /// @notice Burn tokens.
+    /// @dev Only callable by the owner pod.
+    /// @param account The address to burn tokens from.
+    /// @param amount The amount of tokens to burn.
     function burn(address account, uint256 amount) external onlyOwnerPod {
         _burn(account, amount);
     }
+
+    // The following functions override the base token logic to disable transfers and approvals
+    // They will always revert
 
     function approve(address /* spender */, uint256 /* amount */) public pure override(ERC20, IERC20) returns (bool) {
         revert ApproveDisabled();
