2d046ee748891c5792be51bb38512578971aef45
Denis <zzomrot.ne@gmail.com>
2023-12-18 20:48:06 +0000

    Bump solhint

--- contracts/TokenizedDelegationPlugin.sol
+++ contracts/TokenizedDelegationPlugin.sol
@@ -11,8 +11,8 @@ contract TokenizedDelegationPlugin is ITokenizedDelegationPlugin, DelegationPlug
     error NotRegisteredDelegatee();
     error AlreadyRegistered();
 
-    uint256 public immutable maxSharePlugins;
-    uint256 public immutable sharePluginGasLimit;
+    uint256 public immutable MAX_SHARE_PLUGINS;
+    uint256 public immutable SHARE_PLUGIN_GAS_LIMIT;
 
     mapping(address => IDelegatedShare) public registration;
 
@@ -27,8 +27,8 @@ contract TokenizedDelegationPlugin is ITokenizedDelegationPlugin, DelegationPlug
     }
 
     constructor(string memory name_, string memory symbol_, IERC20Plugins token_, uint256 maxSharePlugins_, uint256 sharePluginGasLimit_) DelegationPlugin(name_, symbol_, token_) {
-        maxSharePlugins = maxSharePlugins_;
-        sharePluginGasLimit = sharePluginGasLimit_;
+        MAX_SHARE_PLUGINS = maxSharePlugins_;
+        SHARE_PLUGIN_GAS_LIMIT = sharePluginGasLimit_;
     }
 
     function delegate(address delegatee) public virtual override(IDelegationPlugin, DelegationPlugin) {
@@ -37,7 +37,7 @@ contract TokenizedDelegationPlugin is ITokenizedDelegationPlugin, DelegationPlug
     }
 
     function register(string memory name_, string memory symbol_) public virtual onlyNotRegistered returns(IDelegatedShare shareToken) {
-        shareToken = new DelegatedShare(name_, symbol_, maxSharePlugins, sharePluginGasLimit);
+        shareToken = new DelegatedShare(name_, symbol_, MAX_SHARE_PLUGINS, SHARE_PLUGIN_GAS_LIMIT);
         registration[msg.sender] = shareToken;
         emit RegisterDelegatee(msg.sender);
     }
